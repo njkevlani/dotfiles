32418ba9927d79f693307f7e88f74a6c3aca106b
Nilesh <nileshkevlani78@gmail.com>
2018-08-05 19:39:02 +0530

    Typo

--- get.sh
+++ get.sh
@@ -46,16 +46,16 @@ EOF
 # Get bash profile
 get_bash() {
   cp $HOME/.bash_profile $dotDirectory/.bash_profile
-  echo "Cpoied $HOME/.bash_profile to $dotDirectory/.bash_profile"
+  echo "Copied $HOME/.bash_profile to $dotDirectory/.bash_profile"
   cp $HOME/.bashrc $dotDirectory/.bashrc
-  echo "Cpoied $HOME/.bashrc to $dotDirectory/.bashrc"
+  echo "Copied $HOME/.bashrc to $dotDirectory/.bashrc"
 }
 
 get_gtk() {
   cp $HOME/.gtkrc-2.0 $dotDirectory/.gtkrc-2.0
-  echo "Cpoied $HOME/.gtkrc-2.0 to $dotDirectory/.gtkrc-2.0"
+  echo "Copied $HOME/.gtkrc-2.0 to $dotDirectory/.gtkrc-2.0"
   cp $HOME/.config/gtk-3.0/settings.ini $dotDirectory/settings.ini
-  echo "Cpoied $HOME/.config/gtk-3.0/settings.ini to $dotDirectory/settings.ini"
+  echo "Copied $HOME/.config/gtk-3.0/settings.ini to $dotDirectory/settings.ini"
 }
 
 # Get font config
@@ -74,22 +74,22 @@ get_panel() {
 
   mkdir -p $dotDirectory/xfconf/xfce-perchannel-xml
   cp $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml $dotDirectory/xfconf/xfce-perchannel-xml/xfce4-panel.xml
-  echo "Cpoied $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml to $dotDirectory/xfconf/xfce-perchannel-xml/xfce4-panel.xml"
+  echo "Copied $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml to $dotDirectory/xfconf/xfce-perchannel-xml/xfce4-panel.xml"
 }
 
 # Get xfce4-terminal
 get_terminal() {
   cp $HOME/.config/xfce4/terminal/terminalrc $dotDirectory/terminalrc
-  echo "Cpoied $HOME/.config/xfce4/terminal/terminalrc to $dotDirectory/terminalrc"
+  echo "Copied $HOME/.config/xfce4/terminal/terminalrc to $dotDirectory/terminalrc"
 }
 
 # Get VSCode settings and keybindings
 get_vscode() {
   mkdir -p $dotDirectory/VSCode
   cp $HOME/.config/Code/User/keybindings.json $dotDirectory/VSCode/keybindings.json
-  echo "Cpoied $HOME/.config/Code/User/keybindings.json to $dotDirectory/.config/Code/User/keybindings.json"
+  echo "Copied $HOME/.config/Code/User/keybindings.json to $dotDirectory/.config/Code/User/keybindings.json"
   cp $HOME/.config/Code/User/settings.json $dotDirectory/VSCode/settings.json
-  echo "Cpoied $HOME/.config/Code/User/settings.json to $dotDirectory/VSCode/settings.json"
+  echo "Copied $HOME/.config/Code/User/settings.json to $dotDirectory/VSCode/settings.json"
 }
 
 # Get IntelliJ settings
@@ -101,30 +101,30 @@ get_intellij() {
 get_keybinding() {
   mkdir -p $dotDirectory/xfconf/xfce-perchannel-xml
   cp $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml $dotDirectory/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
-  echo "Cpoied $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml to $dotDirectory/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml"
+  echo "Copied $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml to $dotDirectory/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml"
 }
 
 get_vim() {
   cp $HOME/.vimrc $dotDirectory/.vimrc
-  echo "Cpoied $HOME/.vimrc to $dotDirectory/.vimrc"
+  echo "Copied $HOME/.vimrc to $dotDirectory/.vimrc"
 }
 
 get_thunar_usa() {
   cp $HOME/.config/Thunar/uca.xml $dotDirectory/uca.xml
-  echo "Cpoied $HOME/.config/Thunar/uca.xml to $dotDirectory/uca.xml"
+  echo "Copied $HOME/.config/Thunar/uca.xml to $dotDirectory/uca.xml"
 }
 
 get_xfce() {
   mkdir -p $dotDirectory/xfconf/xfce-perchannel-xml
   cp $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml $dotDirectory/xfconf/xfce-perchannel-xml/xfwm4.xml
-  echo "Cpoied $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml to $dotDirectory/xfconf/xfce-perchannel-xml/xfwm4.xml"
+  echo "Copied $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml to $dotDirectory/xfconf/xfce-perchannel-xml/xfwm4.xml"
   cp $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml $dotDirectory/xfconf/xfce-perchannel-xml/xsettings.xml
-  echo "Cpoied $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml to $dotDirectory/xfconf/xfce-perchannel-xml/xsettings.xml"
+  echo "Copied $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml to $dotDirectory/xfconf/xfce-perchannel-xml/xsettings.xml"
 }
 
 get_xmodmap() {
   cp $HOME/.Xmodmap $dotDirectory/.Xmodmap
-  echo "Cpoied $HOME/.Xmodmap to $dotDirectory/.Xmodmap"
+  echo "Copied $HOME/.Xmodmap to $dotDirectory/.Xmodmap"
 }
 
 if [ $# -eq 0 ];
