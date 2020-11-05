8f7dd8d6b4164da5f499ffe4a2c011b53fa79e9f
Nilesh <njkevlani@gmail.com>
2020-11-05 17:37:24 +0530

    Added note taker script
    Got it from @CalinLeafshade

---
+++ scripts/note_taker.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+note_file="$HOME/notes/note-$(date +%Y-%m-%d).md"
+echo $note_file
+
+if [ ! -f $note_file ]; then
+  echo "# Notes for $(date +%Y-%m-%d)" > $note_file
+fi
+
+vim -c "norm Go" \
+  -c "norm Go## $(date +%H:%M)" \
+  -c "norm G2o" \
+  -c "norm zz" \
+  -c "startinsert" $note_file
+
