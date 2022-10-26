86d5b0b0c34144de5c0ea10075648bebf5417c05
Nilesh <njkevlani@gmail.com>
2022-10-26 22:52:40 +0530

    [nvim-cmp] use default_capabilities instead of update_capabilities
    Because of a warnning on the startup

--- nvim-lsp/.config/nvim/lua/nvim-cmp.lua
+++ nvim-lsp/.config/nvim/lua/nvim-cmp.lua
@@ -98,6 +98,6 @@ cmp.setup.cmdline(':', {
 })
 
 function GetNvimCmpCapabilities()
-    local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
     return capabilities
 end
