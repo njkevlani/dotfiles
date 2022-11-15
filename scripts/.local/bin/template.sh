76edd60e55f1aae602f716e4059bee28074dd34c
Nilesh <njkevlani@gmail.com>
2022-11-15 22:52:33 +0530

    [scripts] Added a script to create template script
    Most part take from https://sharats.me/posts/shell-script-best-practices/
    This script will copy itself as a new script, which can be used as a
    boilerplate

---
+++ scripts/.local/bin/template.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env sh
+
+# Most part taken from https://sharats.me/posts/shell-script-best-practices/
+
+# Exit when a command fails.
+set -o errexit
+
+# Fail the script when some unset variable is used.
+set -o nounset
+
+# Fail the script when a command in pipe fails.
+set -o pipefail
+
+# To enable script execution with traces, use TRACE=1 ./script.sh
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+run_main() {
+    cmd="${1-}"
+    case $cmd in
+        "" | "-h" | "--help")
+            run_help
+            exit
+            ;;
+        "new")
+            shift
+            run_new $@
+            ;;
+        *)
+            echo "Unknown command."
+            run_help
+            exit 1
+            ;;
+    esac
+}
+
+run_help() {
+    echo 'Create new script with template.
+    Usage: ./template.sh new <new_script_name>
+    Options:
+        -h, --help show this message.'
+}
+
+run_new() {
+    script_abs_path=$(dirname -- "$0")/$(basename -- "$0")
+    cp -i "${script_abs_path}" "$1"
+}
+
+run_main "$@"
