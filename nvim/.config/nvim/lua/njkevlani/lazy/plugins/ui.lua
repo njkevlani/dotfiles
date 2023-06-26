08b0f1cec1281fe2c114a44e1cecc1e0f6c7c855
Nilesh <njkevlani@gmail.com>
2023-06-26 20:40:33 +0530

    [nvim] Add nvim-dev-icons dependency for trouble.nvim

--- nvim/.config/nvim/lua/njkevlani/lazy/plugins/ui.lua
+++ nvim/.config/nvim/lua/njkevlani/lazy/plugins/ui.lua
@@ -71,6 +71,9 @@ return {
   -- better diagnostics list and others
   {
     "folke/trouble.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
     cmd = { "TroubleToggle", "Trouble" },
     opts = { use_diagnostic_signs = true },
     keys = {
