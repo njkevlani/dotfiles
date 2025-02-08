6030c76c30d3ac62107d3fab6d2377f78f4b6c25
Nilesh <njkevlani@gmail.com>
2025-02-08 23:36:26 +0530

    [zsh] made sure syntax highlight plugin is loaded from correct path

--- zsh/.config/zsh/linux.zsh
+++ zsh/.config/zsh/linux.zsh
@@ -1,3 +1,11 @@
-source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+if [ -f  /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
+	# Location of zsh-syntax-highlighting plgin on arch linux.
+	source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+elif [ -f   /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
+	# Location for zsh-syntax-highlighting  plgin on fedora.
+	source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+else
+	echo "did not load zsh-syntax-highlighting"
+fi
 
 source ~/.config/zsh/linux-keybindings.zsh
