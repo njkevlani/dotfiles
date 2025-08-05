5aa8d4df090d2324739a535651169504eaa89453
Nilesh <njkevlani@gmail.com>
2025-08-05 19:54:21 +0530

    [zsh][lazygit] set xdg_config_home
    MacOS does not seem to be doing that by default and lazygit uses a
    different mac specific dir without this variable being set.

--- zsh/.config/zsh/mac.zsh
+++ zsh/.config/zsh/mac.zsh
@@ -6,3 +6,5 @@ PATH="$HOMEBREW_PREFIX/opt/libtool/libexec/gnubin:$PATH"
 source $HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 alias brew_caveats="brew info --json --installed | jq -r 'map(select(.caveats) | \"====\npkg=\(.name)\n\n\(.caveats)\") | .[]'"
+
+export XDG_CONFIG_HOME="$HOME/.config"
