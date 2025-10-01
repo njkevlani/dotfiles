bca876e41ad4b7e68e6442c1573f02bec84ad0a3
Nilesh <njkevlani@gmail.com>
2025-10-01 19:28:44 +0530

    [niri] Use --follow-symlinks option in niri-alt-win-switch script
    Without --follow-symlink, sed would rewrite the file break the symlink.

--- niri/.local/share/bin/niri-alt-win-switch.sh
+++ niri/.local/share/bin/niri-alt-win-switch.sh
@@ -10,11 +10,11 @@ COMMENTED_LINE='            // options "altwin:swap_alt_win"'
 # Check if the line is currently commented
 if grep -q "^$COMMENTED_LINE" "$CONFIG"; then
   # Uncomment the line
-  sed -i "s|$COMMENTED_LINE|$UNCOMMENTED_LINE|" "$CONFIG"
+  sed -i --follow-symlinks "s|$COMMENTED_LINE|$UNCOMMENTED_LINE|" "$CONFIG"
   notify-send "Set swap_alt_win"
 elif grep -q "^$UNCOMMENTED_LINE" "$CONFIG"; then
   # Comment the line
-  sed -i "s|$UNCOMMENTED_LINE|$COMMENTED_LINE|" "$CONFIG"
+  sed -i --follow-symlinks "s|$UNCOMMENTED_LINE|$COMMENTED_LINE|" "$CONFIG"
   notify-send "Unset swap_alt_win"
 else
   echo "Could not find the expected line in $CONFIG."
