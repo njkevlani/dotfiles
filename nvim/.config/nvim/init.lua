b5535c5b9541ba3dc902bad738b85a15c71dd015
Nilesh <njkevlani@gmail.com>
2026-02-21 20:08:48 +0530

    [nvim] use live-preview for markdown preview

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -596,12 +596,9 @@ local plugins = {
   },
 
   {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    ft = { 'markdown' },
-    build = function() vim.fn['mkdp#util#install']() end,
-    keys = {
-      { '<leader>mp', '<cmd>MarkdownPreviewToggle<cr>', desc = 'Toggole NvimTree' },
+    'brianhuster/live-preview.nvim',
+    dependencies = {
+      'folke/snacks.nvim',
     },
   },
 
