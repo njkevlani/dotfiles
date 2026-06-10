2ba2a572c3254c18a70eee4b56742e1776157301
Nilesh <njkevlani@gmail.com>
2026-06-10 19:50:31 +0530

    [zsh] add openlinks alias

--- zsh/.config/zsh/mac.zsh
+++ zsh/.config/zsh/mac.zsh
@@ -5,6 +5,10 @@ PATH="$HOMEBREW_PREFIX/opt/libtool/libexec/gnubin:$PATH"
 
 source $HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
+# Use like
+# echo 'https://google.com
+# https://mail.google.com' | openlinks
+alias openlinks='xargs -n 1 open'
 alias brew_caveats="brew info --json --installed | jq -r 'map(select(.caveats) | \"====\npkg=\(.name)\n\n\(.caveats)\") | .[]'"
 
 export XDG_CONFIG_HOME="$HOME/.config"
