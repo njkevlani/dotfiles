bcd243f51b68ee0e96fec5f9f0b0ef98da822c9c
Nilesh <njkevlani@gmail.com>
2026-08-15 20:25:27 +0530

    [nvim] try out metals v2 for java

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -312,6 +312,31 @@ local plugins = {
   },
 
   {
+    'scalameta/nvim-metals',
+    ft = { 'scala', 'sbt', 'java' },
+    opts = function()
+      local metals_config = require('metals').bare_config()
+      metals_config.on_attach = function(client, bufnr)
+        -- your on_attach function
+      end
+      metals_config.settings = {
+        serverVersion = '2.0.0-M2',
+        serverProperties = { '-Xmx4g' },
+      }
+
+      return metals_config
+    end,
+    config = function(self, metals_config)
+      local nvim_metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = self.ft,
+        callback = function() require('metals').initialize_or_attach(metals_config) end,
+        group = nvim_metals_group,
+      })
+    end,
+  },
+
+  {
     -- For formatting
     'stevearc/conform.nvim',
     dependencies = 'folke/snacks.nvim',
